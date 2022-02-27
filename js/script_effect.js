92eecff794e95db030e42d212cad02caa08f0017
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2022-02-27 16:19:26 +0900

    カーテン長さ調整

--- js/script_effect.js
+++ js/script_effect.js
@@ -1,6 +1,6 @@
 function open_curtain(){
-$("#curtain1").animate({width:20, height:410},1000);
-$("#curtain2").animate({width:20, height:410},1000);
+$("#curtain1").animate({width:20, height:1000},1000);
+$("#curtain2").animate({width:20, height:1000},1000);
 }
 function close_curtain(){
 $("#curtain1").animate({width:365},1000);
