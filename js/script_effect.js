dd8af5421fdf4d34764aa83350047243374fbc13
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2022-02-27 16:15:56 +0900

    カーテン開閉

--- js/script_effect.js
+++ js/script_effect.js
@@ -1,8 +1,8 @@
 function open_curtain(){
-$("#curtain1").animate({width:20},1000);
-$("#curtain2").animate({width:20},1000);
+$("#curtain1").animate({width:20, height:410},1000);
+$("#curtain2").animate({width:20, height:410},1000);
 }
 function close_curtain(){
-$("#curtain1").animate({width:200},1000);
-$("#curtain2").animate({width:200},1000);
+$("#curtain1").animate({width:365},1000);
+$("#curtain2").animate({width:365},1000);
 }
