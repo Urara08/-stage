84687033097e9c60b86fbb36ff78f7f808063e01
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2022-02-27 14:46:33 +0900

    video_loop

---
+++ js/script_effect.js
@@ -0,0 +1,8 @@
+function open_curtain(){
+$("#curtain1").animate({width:20},1000);
+$("#curtain2").animate({width:20},1000);
+}
+function close_curtain(){
+$("#curtain1").animate({width:200},1000);
+$("#curtain2").animate({width:191},1000);
+}
