3c862f88009d037ceef3bea33b149fe41d397789
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2022-02-27 21:44:22 +0900

    読み込んだら幕があく

--- js/script_effect.js
+++ js/script_effect.js
@@ -1,9 +1,7 @@
-function open_curtain(){
-$("#curtain1").animate({width:30, height:1000},1000);
-$("#curtain2").animate({width:20, height:1000},1000);
-}
+//スタートのアニメーション
+$(function() {
 
-function close_curtain(){
-$("#curtain1").animate({width:400, height:1000},1000);
-$("#curtain2").animate({width:400, height:1000},1000);
-}
+	setTimeout(function open_curtain(){
+    $("#curtain1").animate({width:30, height:1000},1000).fadeOut(3000);
+    $("#curtain2").animate({width:30, height:1000},1000).fadeOut(3000);
+})})
