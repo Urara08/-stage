a22aa0c934acfa92a1dc0c973853483dd1a74e91
githubUrara08 <github90606372+Urara08@users.noreply.github.com>
2022-02-27 16:32:50 +0900

    修正

--- js/script_effect.js
+++ js/script_effect.js
@@ -1,8 +1,9 @@
 function open_curtain(){
-$("#curtain1").animate({width:20, height:1000},1000);
+$("#curtain1").animate({width:30, height:1000},1000);
 $("#curtain2").animate({width:20, height:1000},1000);
 }
+
 function close_curtain(){
-$("#curtain1").animate({width:365},1000);
-$("#curtain2").animate({width:365},1000);
+$("#curtain1").animate({width:400, height:1000},1000);
+$("#curtain2").animate({width:400, height:1000},1000);
 }
